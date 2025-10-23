27a43b569324dcca7d5e93cbe5d556a2c4a100cd
Amrr-Maherr <amrr.maherr242gmail.com>
2025-10-23 23:39:39 +0300

    (fix): Prevent white flicker on screen transitions in dark mode

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,44 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { View } from "react-native";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const isDark = colorScheme === "dark";
+  const background = isDark ? "#141718" : "#fff";
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }} initialRouteName="index">
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="index" />
-        <Stack.Screen name="Welcome" />
-        <Stack.Screen name="Register" />
-        <Stack.Screen name="Login" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View style={{ flex: 1, backgroundColor: background }}>
+      <ThemeProvider value={isDark ? DarkTheme : DefaultTheme}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: {
+              backgroundColor: background,
+            },
+            animation: "fade",
+          }}
+          initialRouteName="index"
+        >
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="index" />
+          <Stack.Screen name="Welcome" />
+          <Stack.Screen name="Register" />
+          <Stack.Screen name="Login" />
+        </Stack>
+        <StatusBar style={isDark ? "light" : "dark"} />
+      </ThemeProvider>
+    </View>
   );
 }
