f7df6aec18853c6dc7f376d13977a7d468896a15
Amrr-Maherr <amrr.maherr242gmail.com>
2025-10-25 16:46:33 +0300

    Add chat slice for Google GenAI integration - Implemented chatSlice using Redux Toolkit to handle AI chat interactions. - Added createAsyncThunk 'CreateChat' to send user messages to Google GenAI and store responses. - Managed chat state with loading, error, and chat array. - Added clearChat reducer to reset chat history. - Configured extraReducers to handle pending, fulfilled, and rejected states of AI requests. - Stores both user messages and AI bot responses in state for easy UI rendering.

---
+++ Store/ChatSlice.js
@@ -0,0 +1,60 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({
+  apiKey: "AIzaSyCusVNOoxmvhHpLXH6fPpiuPFiC1EYtHYA",
+});
+
+export const CreateChat = createAsyncThunk("chat/create", async (query) => {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: [
+      {
+        role: "user",
+        parts: [{ text: query }],
+      },
+    ],
+  });
+  return {
+    userMessage: query,
+    botMessage: response.text,
+  };
+});
+
+const initialState = {
+  chat: [],
+  loading: false,
+  error: null,
+};
+
+const chatSlice = createSlice({
+  name: "chat",
+  initialState,
+  reducers: {
+    clearChat: (state) => {
+      state.chat = [];
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(CreateChat.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(CreateChat.fulfilled, (state, action) => {
+        state.loading = null;
+        state.error = false
+        state.chat.push({
+          user: action.payload.userMessage,
+          bot: action.payload.botMessage,
+        });
+      })
+      .addCase(CreateChat.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const { clearChat } = chatSlice.actions;
+export default chatSlice.reducer;
