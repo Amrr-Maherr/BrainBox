195c34f8e2ad3b760d16dce437c8ff8f72ca6a84
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-01-10 21:16:07 +0200

    Fix QueryClient setup in Provider.tsx - create QueryClient instance instead of using hook

--- providers/Provider.tsx
+++ providers/Provider.tsx
@@ -1,15 +1,17 @@
 import {
-  useQueryClient,
+  QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
 export default function Provider({children}:any) {
-     const queryClient = useQueryClient();
     return (
       <>
         <QueryClientProvider client={queryClient}>
           {children}
-          <ReactQueryDevtools initialIsOpen={true} />
+          <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </>
     );
