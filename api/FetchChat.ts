cb3dcfe59d3936d4cc295d197eb7996786514769
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-01-10 21:02:11 +0200

    Add FetchChat API service and Gemini response types

---
+++ api/FetchChat.ts
@@ -0,0 +1,33 @@
+
+import Config from 'react-native-config';
+import axios from "axios";
+import { GeminiResponse } from '@/types/RequsetType';
+const API_KEY = Config.API_KEY
+const FetchChat = async (chat: string): Promise<GeminiResponse | null> => {
+    try {
+        const response = await axios.post(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
+            {
+                "contents": [
+                    {
+                        "parts": [
+                            {
+                                "text": "You are BrainBox, a friendly AI assistant. Always respond in clear, simple English or Arabic and be helpful, polite, and concise."
+                            }
+                        ]
+                    },
+                    {
+                        "parts": [
+                            { "text": chat }
+                        ]
+                    }
+                ]
+            }
+        )
+        return response.data
+    } catch (error: any) {
+        console.error(error);
+        return error;
+    }
+}
+
+export default FetchChat
